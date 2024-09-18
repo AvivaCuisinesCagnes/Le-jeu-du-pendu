e289ff0c360c469941bbcbd66e1caada3f0a260d
AvivaCuisinesCagnes <communication-aviva@outlook.fr>
2024-09-18 16:49:36 +0200

    Update review-modal.js

--- scripts/review-modal.js
+++ scripts/review-modal.js
@@ -5,21 +5,21 @@ document.addEventListener("DOMContentLoaded", function() {
 
     if (localStorage.getItem('reviewed') === 'true') {
         googleReviewModal.style.display = 'none';
-        gameContainer.style.display = 'block'; // Affiche le jeu
+        gameContainer.style.display = 'block'; 
     } else {
         googleReviewModal.style.display = 'flex';
-        gameContainer.style.display = 'none'; // Cache le jeu
+        gameContainer.style.display = 'none'; 
     }
 
-    // Quand l'utilisateur clique sur le bouton "Laisser un avis"
+   
     reviewBtn.addEventListener("click", function() {
-        // Rediriger vers la page d'avis Google
+        
         window.open('https://g.page/r/CYjQMZTs0SgXEBM/review', '_blank');
 
-        // Simuler que l'utilisateur a laissé un avis (stocke l'info dans localStorage)
+       
         localStorage.setItem('reviewed', 'true');
 
-        // Ferme la modale et affiche le jeu
+       
         googleReviewModal.style.display = 'none';
         gameContainer.style.display = 'block';
     });
